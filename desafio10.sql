e63fbb1a7222a8e96c945fd86e83e1a24b131383
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-08-18 15:46:48 -0300

    desafio10

--- desafio10.sql
+++ desafio10.sql
@@ -5,6 +5,7 @@ SELECT
     FROM `SpotifyClone`.songs AS son
         INNER JOIN `SpotifyClone`.history AS his ON his.history_music_id = song_id
         INNER JOIN `SpotifyClone`.users AS us ON  us.user_id = his.history_person_id
-        WHERE us.user_plan_id = 1 OR us.user_plan_id = 3
+        INNER JOIN `SpotifyClone`.plan AS p ON p.plan_id = us.user_plan_id
+        WHERE plan_type = 'gratuito' OR plan_type = 'pessoal'
     GROUP BY nome
     ORDER BY nome;
